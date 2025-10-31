dc2514b3846f27654f756193969bcfdc9ea6b901
VooDoo <122112409+MrVooDooNFT@users.noreply.github.com>
2025-10-31 18:06:49 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/BaseR-BaseApp/', // GitHub Pages dizin adı
+  base: isVercel ? '/' : '/BaseR-BaseApp/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
